cf5601a3df5ab39658c706089ce7503c2ab95953
DarekLabaj <dareklabaj@gmail.com>
2024-04-09 19:43:09 +0200

    first issue

---
+++ Converters/ScaleConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows;
+
+namespace DariuszLabaj.MaterialIo.Converters
+{
+    public class ScaleConverter : MarkupExtension, IValueConverter
+    {
+        private readonly double _scale;
+        public ScaleConverter(double scale)
+        {
+            _scale = scale;
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return DependencyProperty.UnsetValue;
+            if (double.TryParse(value.ToString(), out double inputValue))
+            {
+                return inputValue * _scale;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ UserControll/TextInput.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DariuszLabaj.MaterialIo.UserControll
+{
+    /// <summary>
+    /// Interaction logic for TextInput.xaml
+    /// </summary>
+    public partial class TextInput : UserControl
+    {
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(TextInput), new PropertyMetadata(null));
+        public static readonly DependencyProperty SupportingTextProperty = DependencyProperty.Register("SupportingText", typeof(string), typeof(TextInput), new PropertyMetadata(null));
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public string SupportingText
+        {
+            get { return (string)(GetValue(SupportingTextProperty)); }
+            set { SetValue(SupportingTextProperty, value); }
+        }
+
+        public TextInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
